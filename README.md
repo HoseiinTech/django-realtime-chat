8f32fd5aed8f75a056ebdb5727ac7f4c6e8feaca
HoseiinTech <mahmodian8713@gmail.com>
2023-07-15 01:10:07 +0330

    add requirements & README

---
+++ README.md
@@ -0,0 +1,6 @@
+# Real-Time ChatApplication With Django (Channels)
+A simple chat-application with channels written in **Synchronous** and **WebSocket Protocol**.
+
+---
+
+coming soon ...
--- core/settings.py
+++ core/settings.py
@@ -16,17 +16,23 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'daphne',  # Third party
+    'daphne',
     'django.contrib.staticfiles',
-    'chat.apps.ChatConfig',  # Apps
-    'rest_framework',  # Third party
 ]
+THIRD_PARTY = [
+    'rest_framework',
+]
+PROJECT_APPS = [
+    'chat.apps.ChatConfig',
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
